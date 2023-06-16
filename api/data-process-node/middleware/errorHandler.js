b401a1e0e968754e040cf9827b786c559a0195ee
Lee-XR <xian.rong1124@gmail.com>
2023-06-16 20:28:33 +0800

    removed allow-origin header

--- api/data-process-node/middleware/errorHandler.js
+++ api/data-process-node/middleware/errorHandler.js
@@ -1,8 +1,7 @@
 export function returnError(error, req, res, next) {
-    res.status(500).json({err: error});
-    // if (error.response) {
-    //     res.status(error.response.status).json(error.response.data);
-    // } else {
-    //     res.status(500).json({ error: error.message });
-    // }
+    if (error.response) {
+        res.status(error.response.status).json(error.response.data);
+    } else {
+        res.status(500).json({ error: error.message });
+    }
 }
--- api/skiddle-api-php/index.php
+++ api/skiddle-api-php/index.php
@@ -11,7 +11,7 @@ $origin = $_ENV['NODE_ENV'] === 'production'
             ? $_ENV['SKIDDLE_SDK_ORIGIN_URL_PROD'] 
             : $_ENV['SKIDDLE_SDK_ORIGIN_URL_DEV'];
 
-header('Access-Control-Allow-Origin: ' . $origin);
+// header('Access-Control-Allow-Origin: ' . $origin);
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, x-requested-with');
 
