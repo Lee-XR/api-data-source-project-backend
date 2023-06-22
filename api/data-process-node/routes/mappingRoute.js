ff2e54b0f1da10bcfa650d28fb644b377a211776
Lee-XR <xian.rong1124@gmail.com>
2023-06-22 22:57:04 +0800

    added test get route

--- api/data-process-node/routes/mappingRoute.js
+++ api/data-process-node/routes/mappingRoute.js
@@ -4,3 +4,6 @@ import { mapFields } from '../controllers/mappingController.js';
 export const router = express.Router();
 
 router.post('/:apiName', mapFields);
+router.get('/test', (req, res) => {
+    res.json({test: 'Test Message'});
+})
